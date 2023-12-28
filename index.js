8eb476612c430a38d49419de967117368b749363
Brendan Dempsey <bjd1412@gmail.com>
2023-12-27 23:10:09 -0500

    finished

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(minNumber){
+ 
+  if (minNumber <= 400) {
+    return 'This one is on me!';
+  }else if (minNumber <= 2000) {
+  return 'That will be twenty bucks.';
+  }else if ((minNumber >= 2000) && (minNumber <= 2500)) {
+    return 'I will gladly take your thirty bucks.';
+  }else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  if (destination == 'NYC') {
+    return "Ok, sounds good."
+  } else{
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(Tip){
+  switch (Tip) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you."
+    break;
+    default:
+      return "Bye."
+      break;
+  }
+  
 }
